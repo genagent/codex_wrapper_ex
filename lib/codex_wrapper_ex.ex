af826cbfa0f5e1c8140019ea0d1d70530bc66e5a
Josh Rotenberg <joshrotenberg@gmail.com>
2026-03-30 21:36:51 -0700

    first

---
+++ lib/codex_wrapper_ex.ex
@@ -0,0 +1,18 @@
+defmodule CodexWrapperEx do
+  @moduledoc """
+  Documentation for `CodexWrapperEx`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CodexWrapperEx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/codex_wrapper_ex_test.exs
@@ -0,0 +1,8 @@
+defmodule CodexWrapperExTest do
+  use ExUnit.Case
+  doctest CodexWrapperEx
+
+  test "greets the world" do
+    assert CodexWrapperEx.hello() == :world
+  end
+end
